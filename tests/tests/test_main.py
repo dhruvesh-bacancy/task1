5c9302bbe3a6a449ee2bfc79ff0f30c773385102
dhruvesh-bacancy <dhruvesh.sheladiya@bacancy.com>
2025-04-07 10:27:44 +0530

    Update test_main.py

--- tests/tests/test_main.py
+++ tests/tests/test_main.py
@@ -1,11 +1,1 @@
-from app.main import add, subtract
-
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
-
-def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 5) == -5
-    assert subtract(10, 10) == 0
+#
